99894a232e525e794372cb42637384daf2510ea1
alkeshnagar56 <alkeshnagar56@gmail.com>
2024-09-10 21:18:32 +0530

    login signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
-import Navbar from './Navbar'
+// import Navbar from './Navbar'
 import './App.css'
-import Footer from './Footer'
-import Header from './header'
-import Info_Section from './Info_Section'
+// import Footer from './Footer'
+// import Header from './header'
+// import Info_Section from './Info_Section'
+import Login from './Login'
+import Signup from './signup'
 
 const App = () => {
   return (
-    <>
-        <Navbar />
+    <div className='parent'>
+      <Login />
+      <Signup />
+        {/* <Navbar />
         <Header />
         <Info_Section />
-        <Footer />
-    </>
+        <Footer /> */}
+    </div>
   )
 }
 
